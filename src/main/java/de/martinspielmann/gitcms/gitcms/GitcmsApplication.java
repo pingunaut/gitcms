ff0ca11df734a80f91bf38c474465b178093bb75
Martin Spielmann <mail@martinspielmann.de>
2017-11-27 20:22:22 +0100

    initial commit

---
+++ src/main/java/de/martinspielmann/gitcms/gitcms/GitcmsApplication.java
@@ -0,0 +1,12 @@
+package de.martinspielmann.gitcms.gitcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitcmsApplication.class, args);
+	}
+}
