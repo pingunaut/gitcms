4aa3143b4205549e3b4301db720bd909cd809498
Martin Spielmann <mail@martinspielmann.de>
2017-11-28 21:30:11 +0100

    remove unused import

--- src/main/java/de/martinspielmann/gitcms/gitcms/GitcmsApplication.java
+++ src/main/java/de/martinspielmann/gitcms/gitcms/GitcmsApplication.java
@@ -2,10 +2,8 @@ package de.martinspielmann.gitcms.gitcms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootApplication
-@ContextConfiguration("classpath:/applicationContext-CrowdRestClient.xml")
 public class GitcmsApplication {
 
 	public static void main(String[] args) {
